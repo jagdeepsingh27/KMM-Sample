1caab6e105fa7d3e5235d91fcaf4adf44a6de70b
Jagdeep Singh <132729448+jagdeepsingh27@users.noreply.github.com>
2023-09-15 10:37:54 +0530

    adds ktor dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
+    kotlin("plugin.serialization") version "1.6.0"
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
@@ -26,14 +27,34 @@ kotlin {
     }
 
     sourceSets {
+        val ktorVersion = "2.3.2"
         val commonMain by getting {
             dependencies {
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+            }
+        }
+        val androidMain by getting{
+            dependencies{
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
+                //  api ("io.ktor:ktor-client-android:1.5.0'")
+                //  api ("io.ktor:ktor-client-serialization:1.5.4")
+                //  api ("io.ktor:ktor-client-logging-jvm:1.5.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
+
+            }
+        }
+        val iosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:$ktorVersion")
             }
         }
     }
