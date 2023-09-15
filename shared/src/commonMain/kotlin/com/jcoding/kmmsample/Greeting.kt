5b12f15301eab179dd4cde3d8f57e8df082cf037
Jagdeep Singh <132729448+jagdeepsingh27@users.noreply.github.com>
2023-09-15 10:26:33 +0530

    initial commit

---
+++ shared/src/commonMain/kotlin/com/jcoding/kmmsample/Greeting.kt
@@ -0,0 +1,9 @@
+package com.jcoding.kmmsample
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
